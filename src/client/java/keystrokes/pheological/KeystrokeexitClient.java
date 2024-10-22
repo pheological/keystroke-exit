95880c383e3d7b063b0ca5410572dbdb7f4dd7e0
pheological <162748832+pheological@users.noreply.github.com>
2024-10-22 01:07:34 -0700

    Update KeystrokeexitClient.java

--- src/client/java/keystrokes/pheological/KeystrokeexitClient.java
+++ src/client/java/keystrokes/pheological/KeystrokeexitClient.java
@@ -7,26 +7,37 @@ import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import org.lwjgl.glfw.GLFW;
 import net.minecraft.client.util.InputUtil;
 
-import net.fabricmc.api.ClientModInitializer;
-
 public class KeystrokeexitClient implements ClientModInitializer {
+	private boolean wasWPressed = false;
+	private boolean wasAPressed = false;
+	private boolean wasSPressed = false;
+	private boolean wasDPressed = false;
+
 	@Override
 	public void onInitializeClient() {
 		ClientTickEvents.END_CLIENT_TICK.register(client -> {
 			if (client.currentScreen instanceof HandledScreen) {
 				HandledScreen<?> screen = (HandledScreen<?>) client.currentScreen;
 
-				if (isKeyPressed(GLFW.GLFW_KEY_W) || // Change these to your strafe keys if you don't use WASD
-						isKeyPressed(GLFW.GLFW_KEY_A) ||
-						isKeyPressed(GLFW.GLFW_KEY_S) ||
-						isKeyPressed(GLFW.GLFW_KEY_D)) {
-					client.setScreen(null); //Exits any GUI
+				if (isKeyPressedOnce(GLFW.GLFW_KEY_W, wasWPressed, keyState -> wasWPressed = keyState) || //Change keys to your liking here
+						isKeyPressedOnce(GLFW.GLFW_KEY_A, wasAPressed, keyState -> wasAPressed = keyState) ||
+						isKeyPressedOnce(GLFW.GLFW_KEY_S, wasSPressed, keyState -> wasSPressed = keyState) ||
+						isKeyPressedOnce(GLFW.GLFW_KEY_D, wasDPressed, keyState -> wasDPressed = keyState)) {
+					client.setScreen(null);
 				}
 			}
 		});
 	}
+	private boolean isKeyPressedOnce(int keyCode, boolean wasKeyPreviouslyPressed, java.util.function.Consumer<Boolean> updateKeyState) {
+		boolean isCurrentlyPressed = InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), keyCode);
 
-	private boolean isKeyPressed(int keyCode) {
-		return InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), keyCode);
+		if (isCurrentlyPressed && !wasKeyPreviouslyPressed) {
+			updateKeyState.accept(true);
+			return true;
+		} else if (!isCurrentlyPressed) {
+			updateKeyState.accept(false);
+		}
+
+		return false;
 	}
-	}
+}
